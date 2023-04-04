e01a52c1f999b7872bca4c3b631ab048b7e5c618
andrewfriday13 <110158654+andrewfriday13@users.noreply.github.com>
2023-04-04 14:42:10 +0200

    Update contacstSlice.js

--- src/redux/contacstSlice.js
+++ src/redux/contacstSlice.js
@@ -6,16 +6,16 @@ import storage from 'redux-persist/lib/storage'
 
 
 
-const initialState =[
-    {id: nanoid(), name: 'Rosie Simpson', number: '459-12-56'},
-    {id: nanoid(), name: 'Hermione Kline', number: '443-89-12'},
-    {id: nanoid(), name: 'Eden Clements', number: '645-17-79'},
-    {id: nanoid(), name: 'Annie Copeland', number: '227-91-26'}
-  ]
 
 const contactsSlice = createSlice({
     name: 'contacts',
-    initialState: {contacts:[]},
+    initialState: {contacts:[
+       {id: nanoid(), name: 'Rosie Simpson', number: '459-12-56'},
+    {id: nanoid(), name: 'Hermione Kline', number: '443-89-12'},
+    {id: nanoid(), name: 'Eden Clements', number: '645-17-79'},
+    {id: nanoid(), name: 'Annie Copeland', number: '227-91-26'}
+  
+    ]},
     reducers:{
         addContact:{
             reducer(state, action){
